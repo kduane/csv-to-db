979401d96ad316c04ab3f1f04c84a20a6b3bb87d
kduane <karl.duane@gmail.com>
2018-04-10 13:03:09 -0400

    set up db schema, create ingredients db, got import segment working

--- code.rb
+++ code.rb
@@ -11,3 +11,18 @@ def db_connection
 end
 
 # your code, here
+
+#import data from the CSV file
+db_connection do |conn|
+  CSV.foreach("ingredients.csv", a+) do |row|
+    ingredient = row[1]
+    # export to PSQL database
+    conn.exec_params(
+      'INSERT INTO ingredients (ingredient) VALUES ($1)',
+      [ingredient]
+    )
+  end
+end
+
+# pull from database & output on screen
+db_connection do |conn|
---
+++ schema.sql
@@ -0,0 +1,6 @@
+DROP TABLE IF EXISTS ingredients;
+
+CREATE TABLE ingredients (
+  id SERIAL PRIMARY KEY,
+  ingredient VARCHAR(255)
+)
